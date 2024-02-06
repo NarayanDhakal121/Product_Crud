0ae0f7d2a360891b0865dcb1ad7812b29edd80c3
NarayanDhakal121 <141776158+NarayanDhakal121@users.noreply.github.com>
2024-02-06 10:33:19 +0545

    Update Layout.tsx

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -7,11 +7,11 @@ import Dashboard from '../Pages/Dashboard'
 
 const Layout = () => {
   return (
-<Grid templateColumns= 'repeat(6 , 1fr)' bg={'grey.400'}>
+<Grid templateColumns= 'repeat(6 , 1fr)' bg={'list.900'}>
 
   <GridItem as={'aside'}
   colSpan={{base:6, lg:2, xl:1}}
-  bg={'red.400'}
+  bg={'sidebar.500'}
   minH={{lg:'100vh'}}
   p={{base:'20px', lg:4, xl:'30px'}} >
  <Sidebar/>
@@ -20,7 +20,7 @@ const Layout = () => {
 
 {/* ----for navbar ----*/}
 
-<GridItem bg={'blue.400'}
+<GridItem 
 as={'nav'}
 colSpan={{base:6, lg:4, xl:5}}
 p={'40px'}>

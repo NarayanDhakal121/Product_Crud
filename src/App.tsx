b38fb4dbf8c08190b14744f2f855b8038a4e5e77
NarayanDhakal121 <141776158+NarayanDhakal121@users.noreply.github.com>
2024-02-14 10:03:19 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter , Routes, Route} from 'react-router-dom'
 import ProductForm from './Pages/ProductForm'
 import Layout from './Layout/Layout'
-import Viewp from './Pages/Viewp'
+import Viewp from './Pages/ProductTable'
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
           <Route path='/' element={<Layout/>} />
           <Route path='/create' element={<ProductForm/>} />
           <Route path='/update/:id' element={<ProductForm/>} />
-          <Route path='/view' element={<Viewp/>} />
+          <Route path='/view' element={<ProductTable/>} />
        
         </Routes>
     </BrowserRouter>
